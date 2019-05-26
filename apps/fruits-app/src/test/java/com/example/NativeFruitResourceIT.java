a8c55f1e5007c6773589e9098ae033cefe4b1dca
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-05-26 19:02:22 +0530

    add hibernate and panache
    - added panache and hibernate chapter
    - using new rhd tutorials ui
    - workshop site added

---
+++ apps/fruits-app/src/test/java/com/example/NativeFruitResourceIT.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeFruitResourceIT extends FruitResourceTest {
+
+    // Execute the same tests but in native mode.
+}
