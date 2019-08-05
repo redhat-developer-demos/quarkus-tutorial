9bd7b0ded8c4bf84f750af769b456c66c2fc2b63
Alex Soto <asotobu@gmail.com>
2019-08-05 13:48:14 +0200

    feat(#7): Adds a custom health check

--- buildDocs.sh
+++ buildDocs.sh
@@ -2,4 +2,4 @@
 
 [[ -d gh-pages ]] && rm -rf gh-pages
 
-docker run -it --rm -v `pwd`:/antora  antora/antora site.yml --stacktrace
+docker run -it --rm -v `pwd`:/antora  antora/antora:2.0.0 site.yml --stacktrace
