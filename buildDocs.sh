e120809690710e9aa751958925cbb1457b22dcc8
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-10-21 12:13:34 +0200

    (fix) site gen script

--- buildDocs.sh
+++ buildDocs.sh
@@ -4,4 +4,4 @@
 
 site=${1:-'site.yml'}
 
-docker run -it --rm -v `pwd`:/antora  antora/antora:2.0.0 site.yml --stacktrace
+docker run -it --rm -v `pwd`:/antora  antora/antora:2.0.0 $site --stacktrace
