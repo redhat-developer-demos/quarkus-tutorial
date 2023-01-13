b4a9e337ec552b92ea783e3b849deca22580942f
Kevin Dubois <kevin.dubois@redhat.com>
2023-01-13 12:01:27 +0100

    Add Quarkus CLI instructions (#101)
    * Add Quarkus CLI option
    
    * Update HelloResource to GreetingResource (default for RestEasy)

--- bin/build-site.sh
+++ bin/build-site.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 docker run -u $(id -u) -v $PWD:/antora:Z --rm -t antora/antora:2.3.1 --cache-dir=./.cache/antora github-pages.yml
+#docker run -v $PWD:/antora:Z --rm -t antora/antora:2.3.1 --cache-dir=./.cache/antora github-pages.yml
